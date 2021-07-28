d52966c52dbc365afcbec94802a072e9bec40f3a
Amelia <atan@ucls.uchicago.edu>
2021-07-28 11:17:43 -0500

    Created Add Items view

---
+++ To-Do List Hackwich/AddItemView.swift
@@ -0,0 +1,34 @@
+//
+//  AddItemView.swift
+//  To-Do List Hackwich
+//
+//  Created by Franklin Tan on 7/28/21.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    @ObservedObject var toDoList: ToDoList
+    @State private var priority = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let priorities = ["High", "Medium", "Low"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Priority", selection: $priority) {
+                    ForEach(Self.priorities, id: \.self) { priority in
+                        Text(priority)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddItemView(toDoList: ToDoList())
+    }
+}
