d102ef676269be36e77413cfbb2c799508b4a603
Amelia <atan@ucls.uchicago.edu>
2021-07-28 10:41:07 -0500

    Initial Commit

---
+++ To-Do List Hackwich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-Do List Hackwich
+//
+//  Created by Franklin Tan on 7/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To-Do List Hackwich/To_Do_List_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_HackwichApp.swift
+//  To-Do List Hackwich
+//
+//  Created by Franklin Tan on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
