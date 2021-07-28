1b180b4623827702248fa7c4e8f0b093e0a1e238
Amelia <atan@ucls.uchicago.edu>
2021-07-28 11:14:32 -0500

    Created custom ToDoList class

---
+++ To-Do List Hackwich/ToDoList.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoList.swift
+//  To-Do List Hackwich
+//
+//  Created by Franklin Tan on 7/28/21.
+//
+
+import Foundation
+
+class ToDoList: ObservableObject {
+    @Published var items = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+                            ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+                            ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+ }
